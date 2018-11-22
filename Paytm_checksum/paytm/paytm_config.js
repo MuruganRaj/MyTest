5bc86188365b962828fca51f6f6d9074eda17d35
Murugan <muruga4046@gmail.com>
2018-11-22 15:09:24 +0530

    Update paytm_config.js

--- Paytm_checksum/paytm/paytm_config.js
+++ Paytm_checksum/paytm/paytm_config.js
@@ -1,9 +1,9 @@
 module.exports = {
   paytm_config: {
-		MID: 'Premat36936345160441',
+		MID: '',
 		WEBSITE: 'https://paytm.com/',
     CHANNEL_ID: 'WAP',
     INDUSTRY_TYPE_ID: 'Retail',
-    MERCHANT_KEY : 'm!#C@oicZ&11S@4W'
+    MERCHANT_KEY : ''
 	}
 }

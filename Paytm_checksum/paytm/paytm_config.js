4df05bfee43f10893d8da87c04e6fb17ac771eb4
Murugan R <murugan@molc.in>
2018-11-22 15:06:24 +0530

    initial commit

---
+++ Paytm_checksum/paytm/paytm_config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  paytm_config: {
+		MID: 'Premat36936345160441',
+		WEBSITE: 'https://paytm.com/',
+    CHANNEL_ID: 'WAP',
+    INDUSTRY_TYPE_ID: 'Retail',
+    MERCHANT_KEY : 'm!#C@oicZ&11S@4W'
+	}
+}
